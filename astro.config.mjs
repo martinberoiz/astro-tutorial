c9c7fb8b45c637bbfd1ac2256f593acb209b3a35
Martin Beroiz <martinberoiz@gmail.com>
2024-11-22 15:49:29 -0800

    Add Preact Greeting island.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://monumental-bavarois-35be4a.netlify.app/",
+  integrations: [preact()],
 });
